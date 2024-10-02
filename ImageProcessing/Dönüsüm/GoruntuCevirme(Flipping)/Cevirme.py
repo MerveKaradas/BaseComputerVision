65d57a8644e0564e5521e53fa445f3cd0e7437b6
Merve Karadaş <100419329+MerveKaradas@users.noreply.github.com>
2024-10-02 15:33:09 +0300

    Update Cevirme.py

--- ImageProcessing/Dönüsüm/GoruntuCevirme(Flipping)/Cevirme.py
+++ ImageProcessing/Dönüsüm/GoruntuCevirme(Flipping)/Cevirme.py
@@ -1,22 +1,20 @@
 import cv2
 
 image = cv2.imread('../img.png')
+
+# Yatay çevirme
+flipped_horizontally = cv2.flip(image, 1)
+
+# Dikey çevirme
+flipped_vertically = cv2.flip(image, 0)
+
+# Hem yatay hem dikey çevirme
+flipped_both = cv2.flip(image, -1)
+
+
 cv2.imshow('Orjinal Resim', image)
-
-# Görüntü boyutları
-(h, w) = image.shape[:2]
-
-# Döndürme merkezi (görüntünün merkezi)
-center = (w // 2, h // 2)
-
-# Döndürme matrisi oluşturma (45 derece döndürme)
-rotation_matrix = cv2.getRotationMatrix2D(center, 45, 1.0)
-
-# Görüntüyü döndürme
-rotated_image = cv2.warpAffine(image, rotation_matrix, (w, h))
-
-cv2.imshow('Dondurulmus resim', rotated_image)
-
-
+cv2.imshow('Yatay Cevirme', flipped_horizontally)
+cv2.imshow('Dikey Cevirme', flipped_vertically)
+cv2.imshow('Hem Yatay Hem Dikey Cevirme', flipped_both)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
